717a16a32ca75cb7236f8c16f433274fa14e6799
sandra1036 <sandraromero262@gmail.com>
2018-10-15 10:19:38 +0200

    JAdivina modificado añadiendo contador de intentos y  limites

--- JAdivina/src/org/institutoserpis/ed/Adivina.java
+++ JAdivina/src/org/institutoserpis/ed/Adivina.java
@@ -8,16 +8,25 @@ public class Adivina {
 	   Random random = new Random();
 	    int numeroAleatorio= random.nextInt(1000)+1;
 	    Scanner scanner=new Scanner(System.in);
-	    System.out.println("Adivina un número (entre 1 y 1000)");
+	    int contador=0;
+	    int minimo=1;
+	    int maximo=1000;
+	    contador++;
+	    System.out.printf("Adivina un número (entre %s y %s)[intento %s]", minimo,maximo,contador);
 	    int num=scanner.nextInt();
 	     while (num !=numeroAleatorio) {
-	    	 if(num<numeroAleatorio)
+	    	 if(num<numeroAleatorio) {
 	    		 System.out.println("Es mayor que ese");
-	    	 else 
+	    		 minimo=num+1;
+	    	 }else {
+	    		 
 	    		 System.out.println("Es menor que ese");
-	    		 System.out.println("Adivina un número (entre 1 y 1000)");
+	    		 maximo=num-1;
+	    	 }
+	    	 contador++;
+	    		 System.out.printf("Adivina un número (entre %s y %s)[intento %s]", minimo,maximo,contador);
 	    		 num=scanner.nextInt();
 	     }
-	   
+	   System.out.println("Enhorabuena has encontrado el número.");
    }
 }
