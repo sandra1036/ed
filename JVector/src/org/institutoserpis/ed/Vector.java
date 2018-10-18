7c2155b240bec8294be3840a5235faddfcff79bc
sandra1036 <sandraromero262@gmail.com>
2018-10-18 12:16:30 +0200

    Añadido JVector

---
+++ JVector/src/org/institutoserpis/ed/Vector.java
@@ -0,0 +1,14 @@
+package org.institutoserpis.ed;
+
+public class Vector {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+       int[] v =new int[] {16,12,15,14,17};
+       int index= indexOf(v,12);
+       System.out.println("index="+index);
+	}
+   public static int indexOF(int[] v, int x ) {
+	   return 3;
+   }
+}
