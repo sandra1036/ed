6a854aa5e254440a2beac161ca2b22e5dc69ad08
sandra1036 <sandraromero262@gmail.com>
2018-11-26 09:51:26 +0100

    Añadido CVector y su test

---
+++ CVector/CVector/Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+
+namespace CVector
+{
+    class Vector
+    {
+        public static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+            Array.ForEach(new int[] { 12, 15, 19, 32, 37 }, item => Console.WriteLine("item " + item));
+        }
+
+        public static int BinarySearch(int[] v, int x){
+            int count = v.Length;
+            if (count == 0)
+                return -1;
+            int primero = 0;
+            int ultimo = count - 1;
+            int medio = (primero + ultimo) / 2;
+            while (primero < ultimo && v[medio] != x){
+                if (...)
+                    primero = medio + 1;
+                else
+                    ultimo = medio - 1;
+                medio = (primero + ultimo) / 2;
+            }
+            return -2;   
+        }
+    
+    }
+}
---
+++ CVector/CVector/VectorTest.cs
@@ -0,0 +1,19 @@
+﻿using NUnit.Framework;
+using System;
+namespace CVector
+{
+    [TestFixture]
+    public class VectorTest
+    {
+        [Test]
+        public void BinarySearch()
+        {
+            Assert.AreEqual(-1, Vector.BinarySearch(new int[] { }, 33));
+            Assert.AreEqual(2, Vector.BinarySearch(new int[] {12,15,19,32,37 }, 19));
+            Assert.AreEqual(0, Vector.BinarySearch(new int[] {12, 1}, 12));
+            Assert.AreEqual(1, Vector.BinarySearch(new int[] {15, 12}, 12));
+            Assert.AreEqual(3, Vector.BinarySearch(new int[] {6,10,11,32}, 32));
+            Assert.AreEqual(0, Vector.BinarySearch(new int[] {37}, 37));
+        }
+    }
+}
