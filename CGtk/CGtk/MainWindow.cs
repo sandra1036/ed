cf7b9b65c2576976774dd054ecb3c8f76a5dbcf2
sandra1036 <sandraromero262@gmail.com>
2018-12-10 09:50:27 +0100

    Actualizado GTKSuma

---
+++ CGtk/CGtk/MainWindow.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using Gtk;
+
+public partial class MainWindow : Gtk.Window
+{
+    public MainWindow() : base(Gtk.WindowType.Toplevel)
+    {
+        Build();
+    }
+
+    protected void OnDeleteEvent(object sender, DeleteEventArgs a)
+    {
+        Application.Quit();
+        a.RetVal = true;
+
+    }
+    protected void OnButtonAceptarClicked(object sender, EventArgs e){
+        string nombre = entryNombre.Text;
+        labelSaludo.Text = "Hola" + nombre;
+        //Console.WriteLine("Click en buttonAceptar con nombre= " + nombre);
+    }
+}
---
+++ CGtkSuma/CGtkSuma/Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using Gtk;
+
+namespace CGtkSuma
+{
+    class Calculadora
+    {
+        public static void Main(string[] args)
+        {
+            Application.Init();
+            MainWindow win = new MainWindow();
+            win.Show();
+            Application.Run();
+
+
+        }
+    }
+}
