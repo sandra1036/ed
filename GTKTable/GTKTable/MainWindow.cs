3509c7251f014f2bbfc1687d0dcc17a5b66e2f43
sandra1036 <sandraromero262@gmail.com>
2018-12-17 09:23:15 +0100

    Añadido GTKTable y GTKBingo

---
+++ GTKTable/GTKTable/MainWindow.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using Gtk;
+
+public partial class MainWindow : Gtk.Window
+{
+    private string jugador = "X";
+    public MainWindow() : base(Gtk.WindowType.Toplevel)
+    {
+        Build();
+        Table table = new Table(3, 3, true);
+        for (int row = 0; row < 3; row++)
+            for (int column = 0; column < 3; column++){
+                Button button = new Button();
+                table.Attach(button, (uint)column, (uint)column + 1, (uint)row, (uint)row + 1);
+                button.Clicked +=delegate {
+                    if (button.Label != null)
+                        return;
+                    button.Label = jugador;
+                    if (jugador == "X")
+                        jugador = "O";
+                    else
+                        jugador = "X";
+                };
+
+
+            }
+
+        vBox.Add(table);
+        table.ShowAll();
+
+
+        //Button[] buttons = new Button[]
+        //{};
+        //foreach(Button in buttons){
+        //    Button.Clicked +=delegate {
+        //        Button.Label = "X";
+              
+          //  }
+       // }
+
+    }
+
+    protected void OnDeleteEvent(object sender, DeleteEventArgs a)
+    {
+        Application.Quit();
+        a.RetVal = true;
+    }
+}
