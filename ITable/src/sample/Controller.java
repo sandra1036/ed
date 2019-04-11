56179dc8e8fb443575cd868a7e2b7573aa069b07
sandra1036 <sandraromero262@gmail.com>
2019-04-11 12:14:00 +0200

    Añadido ITable

---
+++ ITable/src/sample/Controller.java
@@ -0,0 +1,18 @@
+package sample;
+
+import javafx.fxml.FXML;
+import javafx.scene.control.TableColumn;
+import javafx.scene.control.TableView;
+
+import javax.swing.text.TabableView;
+
+public class Controller {
+
+    @FXML private TableView tableView;
+
+    @FXML private TableColumn tableColumnNom;
+
+    @FXML private TableColumn tableColumnTele;
+
+
+}
