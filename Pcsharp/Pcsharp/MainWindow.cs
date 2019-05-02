95a07cf928f8ab2ae0f1c4f30d5dc1695598fc48
sandra1036 <sandraromero262@gmail.com>
2019-05-02 12:14:01 +0200

    Añadido pcsharp Agenda

---
+++ Pcsharp/Pcsharp/MainWindow.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using Gtk;
+
+public partial class MainWindow : Gtk.Window
+{
+    public MainWindow() : base(Gtk.WindowType.Toplevel)
+    {
+        Build();
+        treeView.AppendColumn("Nombre",new CellRendererText(),"text",0);
+        treeView.AppendColumn("Teléfono", new CellRendererText(), "text",1);
+        ListStore listStore= new ListStore(typeof(String),typeof(String));
+        treeView.Model = listStore;
+        listStore.AppendValues("Rosa", "46544687");
+        listStore.AppendValues("Edudardo", "85989965");
+
+    }
+
+    protected void OnDeleteEvent(object sender, DeleteEventArgs a)
+    {
+        Application.Quit();
+        a.RetVal = true;
+    }
+}
