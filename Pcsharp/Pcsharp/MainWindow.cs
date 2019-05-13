6d1d96119eed0b59ba3b54a8dc3c26f734a91c4c
sandra1036 <sandraromero262@gmail.com>
2019-05-13 08:42:13 +0200

    Incorporado Pcsharp

---
+++ Pcsharp/Pcsharp/MainWindow.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using Gtk;
+
+public partial class MainWindow : Gtk.Window
+{
+    public MainWindow() : base(Gtk.WindowType.Toplevel)
+    {
+        Build();
+        treeView.AppendColumn("Nombre",new CellRendererText(),"text",0);
+        treeView.AppendColumn("Teléfono", new CellRendererText(), "text",1);
+        ListStore listStore= new ListStore(typeof(String),typeof(String));
+        treeView.Model = listStore;
+        listStore.AppendValues("Rosa", "46544687");
+        listStore.AppendValues("Edudardo", "85989965");
+        listStore.AppendValues("Sonia", "8598996558");
+
+        newAction.Activated += (sender, e) => {
+
+            vBoxContacto.Visible = true;
+            treeView.Sensitive = false;
+        };
+
+
+
+
+    }
+
+    protected void OnDeleteEvent(object sender, DeleteEventArgs a)
+    {
+        Application.Quit();
+        a.RetVal = true;
+    }
+}
