d226efd8a74848426739f429ae36c26b33465431
sandra1036 <sandraromero262@gmail.com>
2019-05-16 12:21:12 +0200

    Pcsharp implementando editAction

--- Pcsharp/Pcsharp/MainWindow.cs
+++ Pcsharp/Pcsharp/MainWindow.cs
@@ -14,13 +14,33 @@ public partial class MainWindow : Gtk.Window
         listStore.AppendValues("Edudardo", "85989965");
         listStore.AppendValues("Sonia", "8598996558");
 
+        editAction.Sensitive = false;
+
         newAction.Activated += (sender, e) => {
+
 
             vBoxContacto.Visible = true;
             treeView.Sensitive = false;
+
         };
+        treeView.Selection.Changed += (sender, e) =>
+            editAction.Sensitive=treeView.Selection.CountSelectedRows()>0; 
 
+      
+        editAction.Activated += (sender, e) => {
 
+            vBoxContacto.Visible = true;
+            treeView.Sensitive = false;
+
+            treeView.Selection.GetSelected(out TreeIter treeIter);
+
+            String nombre=(String)treeView.Model.GetValue(treeIter, 0);
+            String telefono=(String)treeView.Model.GetValue(treeIter, 1);
+
+            entry1.Text = nombre;
+            entry2.Text = telefono;
+
+        };
 
 
     }
